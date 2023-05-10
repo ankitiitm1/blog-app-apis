035f57ef4ec077f3a99c21df219cf1a742cb7c2f
Ankit Pandey <ankit.iitm1@gmail.com>
2023-05-10 14:44:56 +0530

    Removeall

--- blog-app-apis/src/main/java/com/ankit/blog/repositories/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ankit.blog.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ankit.blog.entities.User;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-
-}
